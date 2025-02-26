f3181ebac2fca4bafad3d79666e15ebd586fea51
demoliang\demoliang <10086ng@10086ng.com>
2025-02-26 11:30:42 +0800

    feat: update cache redis name

--- cache_provider.go
+++ cache_provider.go
@@ -12,17 +12,16 @@ const (
 	AccessTokenKey = "access_token"
 )
 
-// CacheProvider 是缓存提供者的结构体定义
-type CacheProvider struct {
+// RedisCacheProvider 是缓存提供者的结构体定义
+type RedisCacheProvider struct {
 	client *redis.Client
 }
 
-// NewCacheProvider 创建一个新的缓存提供者实例
-func NewCacheProvider(addr string, password string, db int) *CacheProvider {
+// NewRedisCacheProvider 创建一个新的缓存提供者实例
+func NewRedisCacheProvider(addr string, password string) *RedisCacheProvider {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     addr,
 		Password: password, // 没有密码则留空
-		DB:       db,       // 使用默认DB
 	})
 
 	// 测试连接是否成功
@@ -31,22 +30,22 @@ func NewCacheProvider(addr string, password string, db int) *CacheProvider {
 		panic(fmt.Sprintf("无法连接到Redis: %v", err))
 	}
 
-	return &CacheProvider{
+	return &RedisCacheProvider{
 		client: rdb,
 	}
 }
 
 // Set 设置缓存值
-func (cp *CacheProvider) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+func (cp *RedisCacheProvider) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
 	return cp.client.SetNX(ctx, key, value, expiration).Err()
 }
 
 // Get 获取缓存值
-func (cp *CacheProvider) Get(ctx context.Context, key string) (string, error) {
+func (cp *RedisCacheProvider) Get(ctx context.Context, key string) (string, error) {
 	return cp.client.Get(ctx, key).Result()
 }
 
 // Delete 删除缓存值
-func (cp *CacheProvider) Delete(ctx context.Context, key string) error {
+func (cp *RedisCacheProvider) Delete(ctx context.Context, key string) error {
 	return cp.client.Del(ctx, key).Err()
 }
